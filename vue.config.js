03448518c015b1d151c68ebf45a708544f8b25ee
Jože Fortun <joze.fortun@gmail.com>
2019-09-22 22:23:15 +0200

    Add appFlavour to build variables

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const buildVueConfig = require('corteza-webapp-auth/vue.config-builder')
 
 module.exports = buildVueConfig({
+  appFlavour: 'Crust',
   appName: 'auth',
   appLabel: 'Crust Auth',
   theme: 'crust-base',
